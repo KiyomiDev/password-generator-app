27de1588f237fc24fd5f88ea1af48a9c012ff795
KiyomiDev <kiyomi.dev@gmail.com>
2022-12-05 17:31:24 +0200

    Create password strength meter functionality

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ const generateBtn = document.querySelector('.generate-btn');
 const passwordText = document.querySelector('.password-text');
 const checkBoxes = document.querySelectorAll('input[type="checkbox"]');
 const settingsErrMsg = document.querySelector('.settings-err-msg');
-
+const strengthMsg = document.querySelector('.strength .message');
+const strengthBars = document.querySelectorAll('.bars .bar');
+let barsLength = 0;
 
 // Make the range interactive
 lengthInput.addEventListener('input', () => {
@@ -58,4 +60,45 @@ generateBtn.addEventListener('click', () => {
   } else {
     settingsErrMsg.classList.remove('hidden');
   }
+  passwordStrength()
 })
+
+// Strength meter
+function passwordStrength() {
+  const selectedSettingsLength = selectedSettings.length;
+  const settingsScore = selectedSettingsLength * 20;
+  const passwordLengthScore = passwordLength < 15 ? 10 : 20;
+  const totalScore = settingsScore + passwordLengthScore;
+
+  if (totalScore >= 80) {
+    strengthMsg.textContent = 'STRONG';
+    barsLength = 4;
+    changeBarsColor(mainColor)
+  }
+  else if (totalScore > 50) {
+    strengthMsg.textContent = 'MEDIUM';
+    barsLength = 3;
+    changeBarsColor('#f8cb63')
+  } 
+  else {
+    strengthMsg.textContent = 'WEAK';
+    barsLength = 1;
+    changeBarsColor('#fb7a56')
+  }
+}
+
+function changeBarsColor(color) {
+    for (const bar of strengthBars) {
+      bar.style.cssText = `
+      background-color: 'transparent';
+      border-color: '#fff';
+    `
+  }
+
+  for (let i = 0; i < barsLength; i++) {
+    strengthBars[i].style.cssText = `
+    background-color: ${color};
+    border-color: ${color};
+   `
+  }
+}
